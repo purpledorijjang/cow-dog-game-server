7949d5f7417fbdbf517319ec93371c80a7eaa8fd
purpledorijjang <purpledorijjang@gmail.com>
2026-06-14 07:46:37 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
+  allowEIO3: true,
   cors: {
     origin: "*",
     methods: ["GET", "POST"]
